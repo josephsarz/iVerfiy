17ebf29ed257dbad8b1b428a2a2e7b02b24eeacf
femicodes <aghedojoe007@gmail.com>
2018-01-19 00:12:07 +0100

    completed activities

---
+++ app/src/main/java/com/codegene/femicodes/cscproject/model/Report.java
@@ -0,0 +1,122 @@
+package com.codegene.femicodes.cscproject.model;
+
+/**
+ * Created by femicodes on 1/12/2018.
+ */
+
+public class Report {
+    String productName;
+    String productType;
+    String storeLocation;
+    String complainDetails;
+    String complaintName;
+    String complaintPhoneNo;
+    String complaintEmail;
+    String state;
+    String lga;
+    String town;
+
+    public Report(String productName, String productType, String storeLocation, String complainDetails, String complaintName, String complaintPhoneNo, String complaintEmail, String state, String lga, String town) {
+        this.productName = productName;
+        this.productType = productType;
+        this.storeLocation = storeLocation;
+        this.complainDetails = complainDetails;
+        this.complaintName = complaintName;
+        this.complaintPhoneNo = complaintPhoneNo;
+        this.complaintEmail = complaintEmail;
+        this.state = state;
+        this.lga = lga;
+        this.town = town;
+    }
+
+    public Report(String productName, String productType, String storeLocation, String complainDetails, String complaintName, String state, String lga, String town) {
+        this.productName = productName;
+        this.productType = productType;
+        this.storeLocation = storeLocation;
+        this.complainDetails = complainDetails;
+        this.complaintName = complaintName;
+        this.state = state;
+        this.lga = lga;
+        this.town = town;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(String storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public String getComplainDetails() {
+        return complainDetails;
+    }
+
+    public void setComplainDetails(String complainDetails) {
+        this.complainDetails = complainDetails;
+    }
+
+    public String getComplaintName() {
+        return complaintName;
+    }
+
+    public void setComplaintName(String complaintName) {
+        this.complaintName = complaintName;
+    }
+
+    public String getComplaintPhoneNo() {
+        return complaintPhoneNo;
+    }
+
+    public void setComplaintPhoneNo(String complaintPhoneNo) {
+        this.complaintPhoneNo = complaintPhoneNo;
+    }
+
+    public String getComplaintEmail() {
+        return complaintEmail;
+    }
+
+    public void setComplaintEmail(String complaintEmail) {
+        this.complaintEmail = complaintEmail;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getLga() {
+        return lga;
+    }
+
+    public void setLga(String lga) {
+        this.lga = lga;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
